0be21f251749ce17cbbaf04b63f1046f396e79a5
Rachelle <rkozuma@hawaii.edu>
2020-02-25 10:52:45 -1000

    Initial Commit

--- AppBuildOne-Two/ViewController.swift
+++ AppBuildOne-Two/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var LabelOne: UILabel!
+    @IBOutlet weak var TextView: UITextView!
+    
+    
+    override func viewDidLoad()
+    {
+        self.LabelOne.text = "Universtiy of Hawai`i West O`ahu"
+        self.view.backgroundColor = UIColor.gray
+        
+        self.TextView.text = "A four-year, indigenous-serving institution, UH West Oʻahu provides an intellectually challenging higher education experience in a supportive setting, offering more than 30 academic concentrations in six degree programs designed for career success."
+    
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func AboutUHWO(_ sender: Any)
+    {
+        self.LabelOne.text = "About UHWO"
+        self.TextView.text = "A four-year, indigenous-serving institution, UH West Oʻahu provides an intellectually challenging higher education experience in a supportive setting, offering more than 30 academic concentrations in six degree programs designed for career success."
+    }
+    
+    @IBAction func AboutACM(_ sender: Any)
+    {
+        self.TextView.text = "Integrating various media domains, such as design, storytelling, and technology, the general creative media concentration provides study and extended practice in the student’s chosen area of competence. Course requirements address development and maintenance of existing artistic skills; foster support for a broader understanding of related disciplines including humanities and technology; and provide experience in working in multidisciplinary teams on realistic problems."
+    }
+    
 
+    
+    
 }
 
